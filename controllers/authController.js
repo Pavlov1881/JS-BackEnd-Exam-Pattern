faa8b4b8a91304a0abc4896ae07e681415cbd31c
Anatoli Pavlov <18app81@gmail.com>
2023-06-13 17:51:17 +0300

    Add register view

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 
-router.get('/login', (req, res) =>{
+router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', (req, res) => {
+    const { username, email, password, confirmPassword } = req.body;
+});
 
 module.exports = router;
