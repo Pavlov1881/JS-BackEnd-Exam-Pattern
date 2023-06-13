eb83c74b278a6d9d1259cab1ba3ec692a0d5d3ec
Anatoli Pavlov <18app81@gmail.com>
2023-06-13 20:10:58 +0300

    Optional check if user exist

--- services/authService.js
+++ services/authService.js
@@ -1,12 +1,22 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
+// check if the user exist
+// exports.findByUsername = (username) => User.findOne({username}) 
+
 exports.register = async (username, email, password, confirmPassword) => {
 
     //validate password
     if (password !== confirmPassword) {
         throw new Error('Password don`t match!'); 
     }
+
+    // check if the user exist
+    // const existingUser = await this.findByUsername(username);
+    // if (existingUser) {
+    //     throw new Error('This username allready exist!');
+    // }
+
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
