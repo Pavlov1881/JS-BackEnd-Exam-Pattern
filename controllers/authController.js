6eda26f09e6bcfd404e67a11251cfeccc0449a5a
Anatoli Pavlov <18app81@gmail.com>
2023-06-14 18:17:24 +0300

    Add error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const authService = require('../services/authService');
 
-const {isAuthorized} = require('../middlewares/authMiddleware')
+const { isAuthorized } = require('../middlewares/authMiddleware')
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
@@ -10,16 +10,18 @@ router.get('/login', (req, res) => {
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
+    try {
+        // receive token from service
+        const token = await authService.login(email, password);
 
-    // receive token from service
-    const token = await authService.login(email, password);
+        // add token to cookie
+        res.cookie('auth', token);
 
-    // add token to cookie
-    res.cookie('auth', token);
+        res.redirect('/');
 
-
-    res.redirect('/');
-
+    } catch (error) {
+        return res.status(404).render('auth/login', { error: error.message })
+    }
 });
 
 router.get('/register', (req, res) => {
@@ -28,13 +30,18 @@ router.get('/register', (req, res) => {
 
 router.post('/register', async (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
-    await authService.register(username, email, password, confirmPassword);
+    try {
+        await authService.register(username, email, password, confirmPassword);
+
+    } catch (error) {
+
+    }
 
     //! check redirect to ?
     res.redirect('/');
 });
 
-router.get('/logout', isAuthorized, (req, res) => {
+router.get('/logout', (req, res) => {
     res.clearCookie('auth');
     res.redirect('/');
 })
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -8,10 +8,11 @@ exports.authentication = async (req, res, next) => {
 
     // ако има токен
     if (token) {
-
         try {
             const decodedToken = await jwt.verify(token, SECRET);    // декодираме токена и го запазваме в променливa
             req.user = decodedToken;                   // запазваме инфото от токена в request-a
+            res.locals.isAuthenticated = true;
+            res.locals.user = decodedToken;
         } catch (error) {
             res.clearCookie('auth');
             res.status(401).render('home/404');
